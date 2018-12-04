1ca0d13ef537e9b0a83b57fe321d2f48b94cc63e
Daniel Bailey <daniel.justin.bailey@gmail.com>
2018-12-03 23:28:24 -0700

    finished styling cards

--- js/script.js
+++ js/script.js
@@ -1,15 +1,37 @@
 document.addEventListener('DOMContentLoaded', () => {
-let addNewCardButton = document.querySelector('.add-new-card');
-let overlay = document.querySelector('.new-card-overlay');
-let closeOverlay = document.querySelector('.close-menu');
+    let overlay = document.querySelector('.overlay');
+    let cardArray = [];
+    let frontCardInput = document.querySelector('#front-card-input');
+    let backCardInput = document.querySelector('#back-card-input');
 
-addNewCardButton.addEventListener('click', () => {
-    overlay.style.display = "flex";
-    closeOverlay.addEventListener('click', () => {
-        overlay.style.display = "none";
+    // Flip Card Over
+    // let card = document.querySelector('.card');
+    // card.addEventListener('click', function() {
+    //     card.classList.toggle('is-flipped');
+    // })
+
+    // OPEN AND CLOSE OVERLAY
+    let openOverlay = document.querySelector('.add-card-button');
+    let closeOverlay = document.querySelector('.close-overlay');
+    openOverlay.addEventListener('click', () => {
+        overlay.style.display = "flex";
+        closeOverlay.addEventListener('click', () => {
+            overlay.style.display = "none";
+        });
     });
+
+    // DELETE ALL CARDS
+    let deleteCards = document.querySelector('.delete-cards');
+    deleteCards.addEventListener('click', () => {
+        alert('All cards removed!');
+        cardArray = [];
+    });
+
+    // MAKE NEW CARD ON FORM SUBMIT
+    let cardForm = document.querySelector('.add-card-form');
+    cardForm.addEventListener('submit', () => {
+
+
+    });
+
 });
-
-
-
-}); // END DOM Function
